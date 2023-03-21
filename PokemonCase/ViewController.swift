3727b09beab29ff01ec02c4318a6383cd0c8fa80
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2023-03-21 21:32:50 +0300

    First commit

---
+++ PokemonCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonCase
+//
+//  Created by Furkan Erzurumlu on 21.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
